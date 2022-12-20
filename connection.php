7190434f8194c8455a2271d9ae7592d601a3a0c4
felixyuri25 <ysantos@xcapitalbank.com.br>
2022-12-20 13:59:15 -0300

    linha de local comentada

--- connection.php
+++ connection.php
@@ -16,7 +16,7 @@ class Database
         try
         {
             self::$db = new PDO('mysql:host='.$db_host.'; port=5835; dbname='.$db_name,$db_password);
-            self::$db = new PDO("$db_driver:host=$db_host; dbname=$db_name", $db_user, $db_password, $options);
+            // self::$db = new PDO("$db_driver:host=$db_host; dbname=$db_name", $db_user, $db_password, $options);
             self::$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
             self::$db->exec('SET NAMES utf8mb4');
